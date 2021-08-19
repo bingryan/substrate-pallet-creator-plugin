9df8bd21d69da810fb8146327b793df8308d0f48
bingryan <bingryan@outlook.com>
2021-08-20 01:01:33 +0800

    init PalletCreatorAction

---
+++ src/main/java/PalletCreatorAction.java
@@ -0,0 +1,12 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+
+public class PalletCreatorAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        Messages.showInfoMessage("Create substrate pallet", "Hello");
+    }
+}
